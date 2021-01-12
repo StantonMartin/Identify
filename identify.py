bc0db8e9b89d4c9ddbd480f6a793c90c694c7ec1
StantonMartin <62398353+StantonMartin@users.noreply.github.com>
2021-01-12 14:29:10 -0500

    Add files via upload

--- identify.py
+++ identify.py
@@ -4,7 +4,38 @@ import geojson
 import sys
 import asyncio
 import datetime
+#import pandas as pd
+#import pandasql as ps
 
+
+from tabula.io import read_pdf
+pdf_path = "MCD12_User_Guide_V6.pdf"
+
+dfs = read_pdf(pdf_path, multiple_tables=True, lattice=True, pages="all")
+#for loop code borrowed from https://stackoverflow.com/questions/49733576/how-to-extract-more-than-one-table-present-in-a-pdf-file-with-tabula-in-python
+#print (len(dfs));
+#print (type(dfs[2]));
+#rint (dfs[0].columns);
+table1 = dfs[0].dropna();
+table2 = dfs[1].dropna();
+table3 = dfs[2].dropna();
+table4 = dfs[3].dropna();
+table5 = dfs[4].dropna();
+table6 = dfs[5].dropna();
+table7 = dfs[6].dropna();
+table8 = dfs[7].dropna();
+table9 = dfs[8].dropna();
+table10 = dfs[9].dropna();
+table11 = dfs[10].dropna();
+table12 = dfs[11].dropna();
+
+
+print (table1["Short\rName"]);
+#print (dfs[0]);
+
+for table in dfs:
+    #print (table)
+    pass
 #lat = sys.argv[1];
 
 #long = sys.argv[2];
@@ -33,7 +64,7 @@ header = {'Content-Type':'application/json'};
 response = requests.get('https://modis.ornl.gov/rst/api/v1/MCD12Q1/dates?latitude=39.56499&longitude=-121.55527', headers=header)
 
 dates = json.loads(response.text)['dates']
-print(dates);
+#print(dates);
 modis_dates = [i['modis_date'] for i in dates]
 calendar_dates = [i['calendar_date'] for i in dates]
 
@@ -68,23 +99,21 @@ def get_landcover(lat,long,modisDateQuery):
         res = json.loads(response.content.decode('utf-8'))
         subset = res["subset"]
         for x in subset:
-            band = x["band"]
-            data = x["data"]
-            data = data[0]
+            band = x["band"].strip();
+            band = band.replace('_',' ');
+            print(band);
+            ClassificationType = table1["Description"][table1["Short\rName"][0] == band];
+            print (ClassificationType[0]);
+            #data = data[0]
             #print (band)
-            if band == 'LC_Type1':
-                band ='IGBP'
-                print(band)
-
-                if data == 12:
-                    print ("Croplands")
+            #
 
 
     else:
         return response.status_code
 
 landcover =  get_landcover(lat,long,modisDateQuery);
-print (landcover)
+#print (landcover)
 
 def get_daymet(lat,long,dateStart,dateEnd):
     query = 'https://daymet.ornl.gov/single-pixel/api/data?lat=45.833493&lon=-119.561634&vars=&format=json';
@@ -95,4 +124,4 @@ def get_daymet(lat,long,dateStart,dateEnd):
         return None
 
 daymet = get_daymet(lat,long,dateStart,dateEnd)
-print(daymet);
+#print(daymet);
